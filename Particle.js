789f7e1e8eb2d7c3614c4cc564741752a1922fde
masteraaaa <89093610+masteraaaa@users.noreply.github.com>
2021-10-21 09:02:04 +0100

    Update Particle.js

--- Particle.js
+++ Particle.js
@@ -7,6 +7,7 @@ class Particles {
         this.r = 10;
         this.body = Bodies.circle(x, y, this.r, options);
         //give color property 
+        this.color = color(random(0,255),random(0,255),random(0,255))
         World.add(world, this.body);
     }
     display() {
