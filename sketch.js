2e1ba877eb6dcf9972d391c7e1cdfd6f34321b33
masteraaaa <89093610+masteraaaa@users.noreply.github.com>
2021-10-21 09:21:07 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,33 +14,37 @@ function setup() {
   world = engine.world;
   ground = new Ground(width/2,height,width,20);
 
-  //create division objects
-  for (var k = 0; k <=800; k = k + 80) {
-    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-  }
 
-  //create 1st row of plinko objects
-  for (var j = 75; j <=width; j=j+50) { 
-    plinkos.push(new Plinko(j,75));
-  }
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
 
-  //create 2nd row of plinko objects
-  for (var j = 50; j <=width-10; j=j+50) 
-  {
-    plinkos.push(new Plinko(j,175));
-  }
 
-  //create 3rd row of plinko objects
-  for (var j = 25; j <=width-20; j=j+50)
-  {
-    plinkos.push(new Plinko(j,275))
-  }
-  
-  //create 4th row of plinko objects
-  for (var j = 0; j <=width-30; j=j+50)
-  {
-    plinkos.push(new Plinko(j,375))
-  }
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
 
     
 }
@@ -50,26 +54,26 @@ function setup() {
 function draw() {
   background("black");
   textSize(20)
- 
+ //text("Score : "+score,20,30);
   Engine.update(engine);
   ground.display();
   
-  //display the plinkos 
-  for (var i = 0; i < plinkos.length; i++) {
-    plinkos[i].display();   
-  }
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   if(frameCount%60===0){
+     particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+     score++;
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
    
-  //display the divisions
-  for (var k = 0; k < divisions.length; k++) {
-    divisions[k].display();
-  }
-
-  //create the particles using frameCount
-if(framecount%60===0){
-  particles.push(new particles(random(0,800),0))
-
-  //display the particles 
-for (var k =0; k < particles.length; k++) {
-  particles[k].display();
-
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
 }
